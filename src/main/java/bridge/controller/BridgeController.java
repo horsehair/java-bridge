e32f932fa1292df6f80c54bc482bd1c85688ebca
Woojin Lee <dhkep03@gmail.com>
2022-11-18 21:57:35 +0900

    feat(BridgeController): 의존성 주입 및 시작 로직 구현

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,4 +1,20 @@
 package bridge.controller;
 
+import bridge.domain.BridgeGame;
+import bridge.ui.InputView;
+import bridge.ui.OutputView;
+
 public class BridgeController {
+    private BridgeGame bridgeGame;
+    private InputView inputView;
+    private OutputView outputView;
+
+    public BridgeController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void playGame() {
+
+    }
 }
